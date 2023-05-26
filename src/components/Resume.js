353baf491a4f0e602a8090a0c5035abd5f6c67f8
danielvictory <danieljvictory@gmail.com>
2023-05-26 06:06:16 -0500

    add Resume section with iframe for resume and download button

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,18 +1,30 @@
 import React from 'react'
 
+import "../styles/Resume.css"
+
+import resume from '../assets/Daniel-Victory-Software-Engineer-Resume-2023.pdf'
+
 const Resume = () => {
   return (
-    <div>
+    <div className="resume-container">
         <holder id="resume" style={{
             display:"block",
             position:"relative",
             visibility:"hidden", 
             top: "-50px" 
             }}></holder>
-        <h1>Resume</h1>
-        <iframe title="Daniel-Victory-embedded-resume" src="https://drive.google.com/file/d/1volCxa3eOs-gllZnSJSkwdaKPoUn_8qv/preview" width="640" height="480" allow="autoplay"></iframe>
-    </div>
 
+            <h1>Resume</h1>
+            <div>
+                <a href={resume} download="Resume">
+                    <button>Download Resume!</button>
+                </a>
+            </div>
+            <br />
+
+        <iframe title="Daniel-Victory-embedded-resume" src="https://drive.google.com/file/d/1volCxa3eOs-gllZnSJSkwdaKPoUn_8qv/preview" width="640" height="480" allow="autoplay"></iframe>
+        
+    </div>
   )
 }
 
